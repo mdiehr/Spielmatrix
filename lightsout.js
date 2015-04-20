21d6f0ac17ef98d0eaa3d34d5500fddbd952d371
mdiehr <mdiehr@gmail.com>
2015-04-19 21:20:37 -0400

    Complete lights out example

--- lightsout.js
+++ lightsout.js
@@ -1,38 +1,71 @@
+// LightsOut example for the Spielmatrix game engine
 
-var sm = new Spielmatrix({width:20, height:10, defaultColor: 0x007700});
+(function LightsOut () {
+    // Game variables
+    var w = h = 5;
+    var colors = [0x000000, 0xFFFF00];
+    var playing = true;
 
-(function lightsOut (sm) {
-    // Initialization
-    var isDown = false;
-    var colors = [0x5D0776, 0xEC8A49, 0xAF3666, 0xF6C84C, 0x4C779A];
-    var colorCount = 0;
-    var color = 0;
+    // Initialize engine
+    var sm = new Spielmatrix({width:w, height:h, defaultColor: colors[1]});
 
-    // Fill grid
-    for (var y = 0; y < 10; ++y) {
-        for (var x = 0; x < 20; ++x) {
-            // sm.draw(x, y, 0xAAAAAA);
+    sm.mousedown = function(x, y) {
+        if (playing) {
+            // Toggle the 5 tiles
+            toggle(x, y);
+            toggle(x-1, y);
+            toggle(x+1, y);
+            toggle(x, y-1);
+            toggle(x, y+1);
+            if (didWin()) {
+                // Won game
+                playing = false;
+                drawSmiley(0x009900);
+            }
+        } else {
+            // Reset
+            playing = true;
+            drawAll(colors[1]);
+        }
+    };
+
+    function drawAll(color) {
+        for (var y = 0; y < h; ++y) {
+            for (var x = 0; x < w; ++x) {
+                sm.draw(x, y, color);
+            }
         }
     }
 
-    sm.mousedown = function(x, y) {
-        color = colors[++colorCount%colors.length];
-        isDown = true;
-        sm.draw(x, y, color);
-    };
-
-    sm.mouseup = function(x, y) {
-        isDown = false;
-        sm.draw(x, y, color);
-    };
-
-    sm.mousemove = function(x, y) {
-        if(!isDown) return;
-        sm.draw(x, y, color);
-    };
-
-    sm.update = function(data) {
-        console.log("Test");
+    function toggle(x, y) {
+        if (x >= 0 && x < w && y >= 0 && y < h) {
+            var tile = sm.tile(x, y);
+            if (tile.color == colors[0]) {
+                tile.setColor(colors[1]);
+            } else {
+                tile.setColor(colors[0]);
+            }
+        }
     }
 
-})(sm);
+    function didWin() {
+        for (var y = 0; y < h; ++y) {
+            for (var x = 0; x < w; ++x) {
+                if (sm.tile(x, y).color == colors[1])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    function drawSmiley(color) {
+        sm.draw(1, 1, color);
+        sm.draw(3, 1, color);
+        sm.draw(0, 3, color);
+        sm.draw(1, 4, color);
+        sm.draw(2, 4, color);
+        sm.draw(3, 4, color);
+        sm.draw(4, 3, color);
+    }
+
+})();
---
+++ test.js
@@ -0,0 +1,28 @@
+(function TestGame () {
+    var sm = new Spielmatrix({width:10, height:10, defaultColor: 0x007700});
+    // Initialization
+    var isDown = false;
+    var colors = [0x5D0776, 0xEC8A49, 0xAF3666, 0xF6C84C, 0x4C779A];
+    var colorCount = 0;
+    var color = 0;
+
+    sm.mousedown = function(x, y) {
+        color = colors[++colorCount%colors.length];
+        isDown = true;
+        sm.draw(x, y, color);
+    };
+
+    sm.mouseup = function(x, y) {
+        isDown = false;
+        sm.draw(x, y, color);
+    };
+
+    sm.mousemove = function(x, y) {
+        if(!isDown) return;
+        sm.draw(x, y, color);
+    };
+
+    sm.update = function(data) {
+        console.log("Test");
+    }
+})();
