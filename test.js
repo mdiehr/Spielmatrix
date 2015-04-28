7bb15d4a8d758a8d262f171397ce6ad115142cb2
mdiehr <mdiehr@gmail.com>
2015-04-27 20:13:36 -0400

    Glyph API

--- test.js
+++ test.js
@@ -5,49 +5,42 @@
     var colors = [0x5D0776, 0xEC8A49, 0xAF3666, 0xF6C84C, 0x4C779A];
     var colorCount = 0;
     var color = 0;
+    var glyph = 0;
+    var bg = 0;
+
     var sm = new Spielmatrix({
-        width:10,
-        height:10,
+        width:20,
+        height:20,
         defaultColor: 0x007700,
-        tileSize: 64,
+        tileSize: 32,
         mousedown : function(x, y) {
-            drawAll(colors[++colorCount%colors.length]);
-            color = colors[++colorCount%colors.length];
             isDown = true;
-            sm.draw(x, y, color);
+            chooseRandom();
+            sm.draw(x, y, bg);
+            sm.glyph(x, y, glyph);
+            sm.glyphColor(x, y, color);
         },
         mouseup : function(x, y) {
             if (isDown) {
                 isDown = false;
-                sm.draw(x, y, color);
             }
         },
         mousemove : function(oldX, oldY, x, y) {
             // sm.log(["mousemove", oldX, oldY, x, y].join(", "));
             if(isDown) {
-                sm.draw(x, y, color);
+                sm.draw(x, y, bg);
+                sm.glyph(x, y, glyph);
+                sm.glyphColor(x, y, color);
             }
         },
         mouseenter : function(x, y) {
             // sm.log(["mouseenter", x, y].join(", "));
-            if(!isDown) {
-                sm.draw(x, y, 0xFFFFFF);
-            }
         },
         mouseleave : function(x, y) {
             // sm.log(["mouseleave", x, y].join(", "));
-            if(!isDown) {
-                sm.draw(x, y, 0x444444);
-            }
         },
         mouseout : function(x, y) {
             // sm.log(["mouseout", x, y].join(", "));
-            if (isDown) {
-                isDown = false;
-                sm.draw(x, y, color);
-            } else {
-                sm.draw(x, y, 0x000000);
-            }
         },
         mouseover : function(x, y) {
             // sm.log(["mouseover", x, y].join(", "));
@@ -66,8 +59,20 @@
     function drawAll(color) {
         for (var y = 0; y < sm.options.height; ++y) {
             for (var x = 0; x < sm.options.width; ++x) {
-                sm.draw(x, y, color);
+                sm.draw(x, y, bg);
+                sm.glyph(x, y, glyph);
+                sm.glyphColor(x, y, color);
             }
         }
     }
+
+    function chooseRandom() {
+        color = sm.randBetween(0x0, 0xFFFFFF);
+        glyph = sm.randBetween(0, 255);
+        bg = sm.randBetween(0x0, 0xFFFFFF);
+    }
+    
+    chooseRandom();
+    drawAll(color);
+
 })();
