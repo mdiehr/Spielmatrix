974127672c25a6c6e4315df6ef2c633f3f3b0f20
mdiehr <mdiehr@gmail.com>
2015-05-03 13:01:02 -0400

    Update sprite sheet

---
+++ bin/encodePETSCII.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+var base64str = base64_encode('./assets/PETSCII.png');
+console.log(base64str);
