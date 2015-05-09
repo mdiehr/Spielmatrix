212a1dd2fbc50660427755006ce87469333424b8
mdiehr <mdiehr@gmail.com>
2015-05-09 15:01:37 -0400

    Update template to use upper case SM

--- template/game.js
+++ template/game.js
@@ -1,50 +1,48 @@
 // Game.js template for Spielmatrix
 (function(){
-    var sm = new Spielmatrix({
+    var SM = new Spielmatrix({
         place : document.getElementById('game'),
         width : 10,
         height : 10,
         defaultColor : 0x000000,
         tileSize : 32,
         mousedown : function(x, y) {
-            // sm.log(["mousedown", x, y].join(", "));
-            sm.glyph(x, y, 0xb5);
-            sm.glyphColor(x, y, 0x00FFFF);
+            // SM.log(["mousedown", x, y].join(", "));
+            SM.glyph(x, y, 0xb5);
+            SM.glyphColor(x, y, 0x00FFFF);
         },
         mouseup : function(x, y) {
-            // sm.log(["mouseup", x, y].join(", "));
+            // SM.log(["mouseup", x, y].join(", "));
         },
         mousemove : function(oldX, oldY, x, y) {
-            // sm.log(["mousemove", oldX, oldY, x, y].join(", "));
+            // SM.log(["mousemove", oldX, oldY, x, y].join(", "));
         },
         mouseenter : function(x, y) {
-            // sm.log(["mouseenter", x, y].join(", "));
+            // SM.log(["mouseenter", x, y].join(", "));
         },
         mouseleave : function(x, y) {
-            // sm.log(["mouseleave", x, y].join(", "));
+            // SM.log(["mouseleave", x, y].join(", "));
         },
         mouseout : function(x, y) {
-            // sm.log(["mouseout", x, y].join(", "));
+            // SM.log(["mouseout", x, y].join(", "));
         },
         mouseover : function(x, y) {
-            // sm.log(["mouseover", x, y].join(", "));
+            // SM.log(["mouseover", x, y].join(", "));
         },
         update : function(data) {
             // drawAll(colors[++colorCount%colors.length]);
         },
         keydown : function(key) {
-            // sm.log(["keydown", key].join(", "));
+            // SM.log(["keydown", key].join(", "));
         },
         keyup : function(key) {
-            // sm.log(["keyup", key].join(", "));
+            // SM.log(["keyup", key].join(", "));
         }
     });
 
+    // Get a reference to the tile selector
+    var S = SM.selector();
     // Draw a glyph & glyph in every bead 
-    for (var x = 0; x < sm.options.width; ++x) {
-        for (var y = 0; y < sm.options.height; ++y) {
-            sm.glyph(x, y, 0xFC);
-            sm.glyphColor(x, y, 0x232355);
-        }
-    }
+    S().glyph(0xFC).glyphColor(0x232355);
+
 })();
