8a368307a7681beefce361e4cc7a95dd3cd29dda
mdiehr <mdiehr@gmail.com>
2015-05-25 12:46:22 -0400

    Audio in template, build script, linting

--- template/game.js
+++ template/game.js
@@ -9,6 +9,7 @@
             // SM.log(["mousedown", x, y].join(", "));
             SM.glyph(x, y, 0xb5);
             SM.glyphColor(x, y, 0x00FFFF);
+            SM.play('Pickup_Coin143');
         },
         mouseup : function(x, y) {
             // SM.log(["mouseup", x, y].join(", "));
